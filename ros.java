f372cf41ea22599ef8c61ffc6471d817e320436a
Saurabh Rajbhar <saurabhrajbhar100@gmail.com>
2026-03-17 10:22:48 +0530

    Add files via upload

---
+++ ros.java
@@ -0,0 +1,5 @@
+class ros {
+	public static void main(String[] args) {
+		System.err.println("Helo jii");
+	}
+}
---
+++ user.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class user{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+
+            int sum = a + b;
+
+            System.out.println("Sum = " + sum);
+
+            sc.close();
+        }
+    }
+}
